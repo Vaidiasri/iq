cf875b566f671de3504ec1ced4b831f244bcebfc
Vaibhav Ghildiyal <vaibhavghildiyal2101@gmail.com>
2025-12-27 13:07:52 +0530

    add the logic  to  protect  the  route

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
+import {clerkMiddleware} from "@clerk/express"
 const app = express();
 const __dirname = path.resolve();
 
@@ -14,10 +15,20 @@ app.use(express.json())
 // credentials true => means  server  allows a  browser to includes cookieson request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use(clerkMiddleware()) // this add auth field to request object 
 
 app.get("/health", (req, res) => {
   res.status(200).json({ mess: "api is up and running" });
 });
+// protected route 
+// app.get("/video-call",protecRoute,(req,res)=>{
+//   try{
+//     res.status(200).json({message:"Video call"})  
+//   }
+//   catch(error){
+//     res.status(500).json({message:"Internal Server Error"})
+//   }
+// })
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/project/dist")));
