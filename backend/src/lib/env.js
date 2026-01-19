8a7e769b5263386e7370165a521786c93611d1e8
Vaibhav Ghildiyal <vaibhavghildiyal2101@gmail.com>
2026-01-19 07:08:08 +0530

    feat: Add environment variable management and configuration using dotenv.

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,7 +4,7 @@ dotenv.config({ quiet: true });
 
 export const ENV = {
   PORT: process.env.PORT,
-  DB_URL: process.env.DB_URL,
+  DB_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   CLIENT_URL: process.env.CLIENT_URL,
   INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
